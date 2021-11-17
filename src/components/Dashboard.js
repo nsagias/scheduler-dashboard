6b14ff918c9bd5a376679f8e7fa01c3add07a806
Nick <nsagias@gmail.com>
2021-11-17 03:22:07 -0500

    import helpers and add helpers to static/mock data as live data and rename value to getValue

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,6 +3,12 @@ import Loading from "./Loading";
 import Panel from "./Panel";
 import axios from "axios";
 import classnames from "classnames";
+import {
+  getTotalInterviews,
+  getLeastPopularTimeSlot,
+  getMostPopularDay,
+  getInterviewsPerDay
+ } from "helpers/selectors";
 
 
 
@@ -10,22 +16,22 @@ const data = [
   {
     id: 1,
     label: "Total Interviews",
-    value: 6
+    getValue: getTotalInterviews
   },
   {
     id: 2,
     label: "Least Popular Time Slot",
-    value: "1pm"
+    getValue: getLeastPopularTimeSlot
   },
   {
     id: 3,
     label: "Most Popular Day",
-    value: "Wednesday"
+    getValue: getMostPopularDay
   },
   {
     id: 4,
     label: "Interviews Per Day",
-    value: "2.3"
+    getValue: getInterviewsPerDay
   }
 ];
 
